c4a6477d43a04d05a455daf582360b83e77ca1b4
Byeolhee-Kim <42535803+kimbyeolhee@users.noreply.github.com>
2022-03-10 11:11:41 +0900

    Add files via upload

---
+++ React/useContext.js
@@ -0,0 +1,46 @@
+import './App.css';
+import {createContext, useContext} from 'react';
+
+const themeDefault = {
+  border:'10px solid blue'
+}
+const themeContext = createContext(themeDefault);
+
+function App() {
+  return (
+    <themeContext.Provider value={{border:'10px dotted green'}}>
+      <div>
+        <h1>Root</h1>
+        <Child1></Child1>
+      </div>
+    </themeContext.Provider>
+  );
+}
+function Child1(props){
+  const theme = useContext(themeContext);
+  return (
+    <div style={{border:theme.border}}>
+      <h1>1</h1>
+      <themeContext.Provider value={{border:'10px dashed blue'}}>
+        <Child2></Child2>
+      </themeContext.Provider>
+    </div>
+  )
+}
+function Child2(props){
+  return (
+    <div>
+      <h1>2</h1>
+      <Child3></Child3>
+    </div>
+  )
+}
+function Child3(props){
+  const theme = useContext(themeContext);
+  return (
+    <div style={{border:theme.border}}>
+      <h1>3</h1>
+    </div>
+  )
+}
+export default App;
